3a9f4a7b05e5f2b90f73ec8cc0fd5eea600c65ff
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-14 12:02:01 +0530

    initial commit

---
+++ src/main/java/com/somsubhra/springdatajpaamigoscode/SpringDataJpaAmigoscodeApplication.java
@@ -0,0 +1,13 @@
+package com.somsubhra.springdatajpaamigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaAmigoscodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaAmigoscodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/somsubhra/springdatajpaamigoscode/SpringDataJpaAmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somsubhra.springdatajpaamigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaAmigoscodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
