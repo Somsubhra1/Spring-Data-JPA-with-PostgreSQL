cc6ee84eaadc50ad0d472167c2cd8ed1855a8ec8
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-15 12:08:53 +0530

    added command line runner to add data at runtime

--- src/main/java/com/somsubhra/springdatajpaamigoscode/SpringDataJpaAmigoscodeApplication.java
+++ src/main/java/com/somsubhra/springdatajpaamigoscode/SpringDataJpaAmigoscodeApplication.java
@@ -1,13 +1,25 @@
 package com.somsubhra.springdatajpaamigoscode;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringDataJpaAmigoscodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaAmigoscodeApplication.class, args);
+    }
+
+
+    //    Just to have few data in db on running
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student maria = new Student("Maria", "James", "maria@gmail.com", 20);
+            studentRepository.save(maria);
+        };
     }
 
 }
