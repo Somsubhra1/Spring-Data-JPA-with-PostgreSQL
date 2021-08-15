92cdda588a4d5d8fba4e971f094137fee40af138
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-15 12:06:56 +0530

    updated Student constructor, removed id field

--- src/main/java/com/somsubhra/springdatajpaamigoscode/Student.java
+++ src/main/java/com/somsubhra/springdatajpaamigoscode/Student.java
@@ -52,8 +52,7 @@ public class Student {
     )
     private Integer age;
 
-    public Student(Long id, String firstName, String lastName, String email, Integer age) {
-        this.id = id;
+    public Student(String firstName, String lastName, String email, Integer age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
